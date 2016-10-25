e7af18f1fe1f8676ba041e4b490c33725cc256e4
Lee Briggs <lbriggs@apptio.com>
2016-10-25 10:46:14 +0100

    Change puppetserver jvm size

--- modules/roles/manifests/puppetmaster.pp
+++ modules/roles/manifests/puppetmaster.pp
@@ -3,11 +3,13 @@ class roles::puppetmaster inherits roles::base {
 
   # set up a puppetmaster
   class { '::puppet':
-  	server                => true,
-  	server_foreman        => false,
-  	server_reports        => 'store',
-  	server_external_nodes => '',
-  	server_environments   => [],
+  	server                    => true,
+  	server_foreman            => false,
+  	server_reports            => 'store',
+  	server_external_nodes     => '',
+  	server_environments       => [],
+    server_jvm_min_heap_size  => '512m',
+    server_jvm_max_heap_size  => '512m'
 	}
 
 }
