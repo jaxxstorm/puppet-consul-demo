85646bc2308d081a7e007ad9661a20603b47cf56
Lee Briggs <lbriggs@apptio.com>
2016-10-25 11:00:21 +0100

    Don't set a common modules path

--- modules/roles/manifests/puppetmaster.pp
+++ modules/roles/manifests/puppetmaster.pp
@@ -3,13 +3,14 @@ class roles::puppetmaster inherits roles::base {
 
   # set up a puppetmaster
   class { '::puppet':
-  	server                    => true,
-  	server_foreman            => false,
-  	server_reports            => 'store',
-  	server_external_nodes     => '',
-  	server_environments       => [],
-    server_jvm_min_heap_size  => '512m',
-    server_jvm_max_heap_size  => '512m'
+  	server                      => true,
+  	server_foreman              => false,
+  	server_reports              => 'store',
+  	server_external_nodes       => '',
+  	server_environments         => [],
+    server_common_modules_path  => [],
+    server_jvm_min_heap_size    => '512m',
+    server_jvm_max_heap_size    => '512m'
 	}
 
 }
