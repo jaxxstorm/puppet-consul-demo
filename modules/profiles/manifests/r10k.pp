e894922fa2c034229d97ce269d58b1f5f414a64f
Lee Briggs <lbriggs@apptio.com>
2016-10-26 14:19:27 +0100

    Change logdir

--- modules/profiles/manifests/r10k.pp
+++ modules/profiles/manifests/r10k.pp
@@ -3,7 +3,7 @@ class profiles::r10k {
 
   cron {'r10k_deploy_environment':
   	ensure  => present,
-    command => 'r10k deploy environment -p -c /etc/puppetlabs/r10k.yaml  --verbose > /var/log/puppet/r10k_deploy_environments.out',
+    command => 'r10k deploy environment -p -c /etc/puppetlabs/r10k.yaml  --verbose > /var/log/puppetlabs/r10k_deploy_environments.out',
     user    => 'root',
     hour    => '*',
     minute  => "*/5",
