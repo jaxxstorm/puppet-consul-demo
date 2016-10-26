dcb44f2683c2f21994b0a8a450997ae720d16930
Lee Briggs <lbriggs@apptio.com>
2016-10-26 19:19:20 +0100

    Some documentation

--- modules/roles/manifests/base.pp
+++ modules/roles/manifests/base.pp
@@ -13,14 +13,22 @@ class roles::base {
     stage => 'pre'
   }
 
+  # We need to make sure our local resolv.conf can also see consul
+  # See hiera for the settings, notice localhost is first
   include ::resolv_conf
 
+  # Unbound allows us to have a local resolver which will forward queries to Consul
   include ::unbound
 
+  # This grabs the current consul nodes using the consul backend:
+  # https://github.com/lynxman/hiera-consul
   $consul_service_array = hiera('consul',[])
+
+  # We than add the addresses of each consul server to a new array
 	$consul_cluster_nodes = consul_info($consul_service_array, 'Address')
 
   unbound::forward { 'service.consul':
+    # We use suffix from stdlib to add the consul DNS port: https://github.com/puppetlabs/puppetlabs-stdlib#suffix
     address => suffix($consul_cluster_nodes, '@8600')
   }
 
