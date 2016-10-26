835bd2ba1a90aec5c6896a46e10031163058008a
Lee Briggs <lbriggs@apptio.com>
2016-10-26 17:14:22 +0100

    Fix typo

--- modules/roles/manifests/consulserver.pp
+++ modules/roles/manifests/consulserver.pp
@@ -10,7 +10,7 @@ class roles::consulserver inherits roles::base {
       ui_dir              => '/opt/consul-ui',
       node_name           => "${::fqdn}",
       log_level           => "DEBUG",
-      encrypt             => "cg8StVXbQJ0gPvMd9o7yrg==" # https://www.consul.io/docs/agent/encryption.html
+      encrypt             => "cg8StVXbQJ0gPvMd9o7yrg==", # https://www.consul.io/docs/agent/encryption.html
       bootstrap_expect    => 3,
       disable_remote_exec => true,
       retry_join          => [ 'consulserver-0.briggs.lan', 'consulserver-1.briggs.lan', 'consuslerver-2.briggs.lan' ]
