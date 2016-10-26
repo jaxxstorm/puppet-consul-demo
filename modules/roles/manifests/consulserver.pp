8086f9c0fd54e2c6af05ea5b4202dc218c2920c0
Lee Briggs <lbriggs@apptio.com>
2016-10-26 17:35:57 +0100

    Move consul server config to profile

--- modules/roles/manifests/consulserver.pp
+++ modules/roles/manifests/consulserver.pp
@@ -1,22 +1,6 @@
 # Configures a consulserver
 class roles::consulserver inherits roles::base {
 
-  class { '::consul':
-    pretty_config => true,
-    config_hash => {
-      datacenter          => "${::digital_ocean_region}",
-      server              => true,
-      data_dir            => '/opt/consul',
-      client_addr         => '0.0.0.0',
-      ui_dir              => '/opt/consul-ui',
-      node_name           => "${::fqdn}",
-      log_level           => "DEBUG",
-      advertise_addr      => "${::ipaddress_eth1}",
-      encrypt             => "cg8StVXbQJ0gPvMd9o7yrg==", # https://www.consul.io/docs/agent/encryption.html
-      bootstrap_expect    => 3,
-      disable_remote_exec => true,
-      retry_join          => [ 'consulserver-0.briggs.lan', 'consulserver-1.briggs.lan', 'consuslerver-2.briggs.lan' ]
-    }
-  }
+  include ::profiles::consul::server
 
 }
