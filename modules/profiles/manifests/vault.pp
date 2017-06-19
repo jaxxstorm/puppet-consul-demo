5fc21454e7053709d86504e5f2a12635ebc582ef
Lee Briggs <lbriggs@apptio.com>
2017-06-19 13:43:44 +0100

    Fix mode and add digital ocean region

--- modules/profiles/manifests/vault.pp
+++ modules/profiles/manifests/vault.pp
@@ -2,44 +2,44 @@ class profiles::vault {
 
   file { '/etc/vault/ssl':
     ensure => directory,
-    mode   => 0700,
+    mode   => '0700',
     owner  => 'vault',
     group  => 'vault',
   }
 
   file { '/etc/vault/ssl/cert.pem':
-    ensure  => present,
+    ensure  => file,
     source  => "/etc/puppetlabs/puppet/ssl/certs/${::fqdn}.pem",
     mode    => '0600',
     owner   => 'vault',
-    mode    => 'vault',
+    group   => 'vault',
     require => File['/etc/vault/ssl'],
   }
 
   file { '/etc/vault/ssl/key.pem':
-    ensure  => present,
+    ensure  => file,
     source  => "/etc/puppetlabs/puppet/ssl/private_keys/${::fqdn}.pem",
     mode    => '0600',
     owner   => 'vault',
-    mode    => 'vault',
+    group   => 'vault',
     require => File['/etc/vault/ssl'],
   }
 
-	class { '::vault':
+  class { '::vault':
     backend  => {
       consul => {
-        address       => "${::fqdn}:8500",
-        path          => "vault",
-      }
+        address => "${::fqdn}:8500",
+        path    => "vault/${::digital_ocean_region}",
+      },
     },
     listener => {
       tcp           => {
-        address       => "0.0.0.0:8200",
+        address       => '0.0.0.0:8200',
         tls_cert_file => '/etc/vault/ssl/cert.pem',
-        tls_key_file  => '/etc/vault/ssl/key.pem', 
-      }
-		},
+        tls_key_file  => '/etc/vault/ssl/key.pem',
+      },
+    },
     require  => [ File['/etc/vault/ssl/cert.pem'], ['/etc/vault/ssl/key.pem'] ],
-	}
+  }
 
 }
