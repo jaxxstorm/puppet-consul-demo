6e91a3641e9c94bfeed446edc83c7b7c4b99cd8e
Lee Briggs <lbriggs@apptio.com>
2017-06-19 11:03:23 +0100

    Change CA droplet size

--- terraform/modules/puppet_ca/inputs.tf
+++ terraform/modules/puppet_ca/inputs.tf
@@ -2,5 +2,5 @@
 variable "digitalocean_domain" {}
 variable "digitalocean_keys" { }
 variable "digitalocean_region" { default = "lon1" }
-variable "digitalocean_droplet_size" { default = "1gb" }
+variable "digitalocean_droplet_size" { default = "2gb" }
 variable "count" { default=1 }
