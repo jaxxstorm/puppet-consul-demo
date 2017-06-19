0d0f84aa3b35557d63d10cf29edbb151d1eeb4f7
Lee Briggs <lbriggs@apptio.com>
2017-06-19 11:10:47 +0100

    Change puppetserver size

--- terraform/modules/puppetserver/inputs.tf
+++ terraform/modules/puppetserver/inputs.tf
@@ -2,6 +2,6 @@
 variable "digitalocean_keys" { }
 variable "digitalocean_domain" { }
 variable "digitalocean_region" { default = "lon1" }
-variable "digitalocean_droplet_size" { default = "1gb" }
+variable "digitalocean_droplet_size" { default = "2gb" }
 variable "count" { default=2 }
 variable "puppet_ca" { type = "list" }
