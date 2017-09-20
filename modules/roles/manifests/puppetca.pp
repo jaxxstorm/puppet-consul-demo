bbe36984a0cff9b3f77212ad72b574d86ff958e3
Lee Briggs <lbriggs@apptio.com>
2017-09-20 11:37:35 -0500

    Change puppetserver healthcheck

--- modules/roles/manifests/puppetca.pp
+++ modules/roles/manifests/puppetca.pp
@@ -27,7 +27,7 @@ class roles::puppetca inherits roles::base {
   ::consul::check { 'puppetserver_healthcheck':
     ensure     => present,
     interval   => '60',
-    script     => "/usr/lib64/nagios/plugins/check_http -H ${::fqdn} -p 8140 -u /production/status/test?environment=production -S -k 'Accept: pson' -s '\"is_alive\":true'",
+    script     => "/usr/lib64/nagios/plugins/check_http -H ${::fqdn} -p 8140 -u /status/v1/services?level=info -S -k 'Accept: pson'",
     notes      => 'Checks the puppetservers\'s status API to determine if the service is healthy',
     service_id => 'puppetserver',
   }
